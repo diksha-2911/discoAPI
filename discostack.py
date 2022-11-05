c4394141faf48313a9edc41ae1ab3229d598cefa
diksha-2911 <90517961+diksha-2911@users.noreply.github.com>
2022-11-05 21:43:23 +0530

    Update discostack.py

--- discostack.py
+++ discostack.py
@@ -1,14 +1,18 @@
 import requests
 
-base_url = "https://api.stackexchange.com/"
-path_url = "/2.3/similar?order=desc&sort=relevance&title={user message}&site=stackoverflow"
+def handle_response(message) -> str:
+    p_message = message.lower()
 
-request_url = base_url + path_url
+    base_url = "https://api.stackexchange.com/"
+    path_url = "/2.3/similar?order=desc&sort=relevance&title=" + p_message+ "&site=stackoverflow"
 
-api_result = requests.get(request_url)
-api_response = api_result.json()
+    request_url = base_url + path_url
 
-for i in range(0, 5):
-    print(api_response['items'][i]['link'])
+    api_result = requests.get(request_url)
+    api_response = api_result.json()
 
+
+    for i in range(0, 5):
+        new_message = api_response['items'][i]['link']
+        return new_message
 
